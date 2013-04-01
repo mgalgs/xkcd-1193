536a35b77092889fca636626b95ffc6e8584ffc2
Mitchel Humpherys <mitchelh@codeaurora.org>
2013-04-01 14:10:39 -0700

    sha1sum seems to have better results than md5sum...

--- 1193.sh
+++ 1193.sh
@@ -1,5 +1,5 @@
 while :; do
-    rand=$(echo $RANDOM | md5sum | cut -f1 -d' ')
+    rand=$(echo $RANDOM | sha1sum | cut -f1 -d' ')
     len=$(shuf -i 3-31 -n 1)
     try=${rand:$len}
     output=$(curl -s -X POST --data "hashable=$try" http://almamater.xkcd.com/?edu=usu.edu | grep -o 'It is off by .* bits.')
