86230e759ee9a0ae19891d6d73e5910b6625a008
Mitchel Humpherys <mitchelh@codeaurora.org>
2013-04-01 15:28:04 -0700

    functional local hashing

--- 1193.py
+++ 1193.py
@@ -11,6 +11,7 @@
 # source skein/bin/activate
 # pip install pyskein
 
+import sys
 import random
 import struct
 import string
@@ -19,29 +20,45 @@ from skein import skein1024
 def hash2bytes(thehash):
     return struct.unpack('128B', bytearray.fromhex(thehash))
 
+THRESHOLD=451                   # from http://almamater.xkcd.com/best.csv (search for usu.edu)
 target = '5b4da95f5fa08280fc9879df44f418c8f9f12ba424b7757de02bbdfbae0d4c4fdf9317c80cc5fe04c6429073466cf29706b8c25999ddd2f6540d4475cc977b87f4757be023f19b8f4035d7722886b78869826de916a79cf9c94cc79cd4347d24b567aa3e2390a573a373a48a5e676640c79cc70197e1c5e7f902fb53ca1858b6'
 target_bytes = hash2bytes(target)
 
-# 0c41172b30c7c7bbc4291ce8e649b3b971a669bd6a0bb411de3076b3fe112a503e34e28292fb6093d82364d9da7a6ba793e42216da7c3e6afc30700cb6f27179485cedb0b5035ebe4fa9eedc2223ae2da28551b4553cec1cdfff62baa52111144163062ccae573ddf60b24de39be4f6088f3d2347882fdd3f3c4d059364ac302
+def sendit(what, offby):
+    print ()
+    print ('FOUND ONE!!!!!!!')
+    print ('GO TRY ' + what)
+    print ('(should be off by %d)' % offby)
+    print ()
 
-def sendit(what, bit_error):
-    print ('Trying ' + what)
+def getbit(b, bitnum):
+    return (b & (1 << bitnum)) >> bitnum
+
+
+print()
+
+nattempts = 0
+nuploads = 0
+best_hash = 100000
 
 while True:
     attempt_len = random.choice(range(50))
     attempt = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(attempt_len))
-    attempt = 'pizza'
     attempt_hash = skein1024(bytes(attempt, 'ascii'), digest_bits=1024).hexdigest()
     attempt_bytes = hash2bytes(attempt_hash)
     nbits = 0
     # see how many bits are different
     for attempt_byte, target_byte in zip(attempt_bytes, target_bytes):
-        mesh = attempt_byte & target_byte
         lbits = 0
         for b in range(8):
-            if not mesh & (1 << b):
+            if getbit(attempt_byte, b) != getbit(target_byte, b):
                 lbits += 1
                 nbits += 1
-        print ('comparing %s to %s %s (%d bits difff)' % (bin(attempt_byte), bin(target_byte), bin(mesh), lbits))
-    print ('%s %s has %d bits different' % (attempt, attempt_hash, nbits))
-    break
+    best_hash = min(best_hash, nbits)
+    if not nattempts % 1000:
+        sys.stdout.write('\r%d hashes compared far (best so far is %d). %d uploaded...   ' % (
+            nattempts, best_hash, nuploads))
+    nattempts += 1
+    if nbits < THRESHOLD:
+        sendit(attempt, nbits)
+        nuploads += 1
