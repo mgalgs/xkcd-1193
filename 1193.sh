a275ee49d7b5bd666a126dd0298f37e27ef3e829
Mitchel Humpherys <mitchelh@codeaurora.org>
2013-04-01 14:06:48 -0700

    shuf bug

--- 1193.sh
+++ 1193.sh
@@ -1,6 +1,6 @@
 while :; do
     rand=$(echo $RANDOM | md5sum | cut -f1 -d' ')
-    len=$(shuf -i 3-32 -n 1)
+    len=$(shuf -i 3-31 -n 1)
     try=${rand:$len}
     output=$(curl -s -X POST --data "hashable=$try" http://almamater.xkcd.com/?edu=usu.edu | grep -o 'It is off by .* bits.')
     echo "$try $output"
