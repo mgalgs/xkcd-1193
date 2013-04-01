49e10dcde1b4d6617b785e1479e41b29680ea855
Mitchel Humpherys <mitchelh@codeaurora.org>
2013-04-01 13:53:48 -0700

    just try randoms

--- 1193.sh
+++ 1193.sh
@@ -1,6 +1,9 @@
 # SUBSTITUE 20000 WITH A RANDOM NUMBER BETWEEN 1-400000
 # When it finishes, choose a new number...
-for try in $(tac /usr/share/dict/words | tail -n +20000 | head -10000); do
-    output=$(curl -s -X POST --data "hashable=${try}" http://almamater.xkcd.com/?edu=usu.edu | grep -o 'It is off by .* bits.')
+while :; do
+    rand=$(echo $RANDOM | md5sum | cut -f1 -d' ')
+    len=$(shuf -i 3-32 -n 1)
+    try=${rand:$len}
+    output=$(curl -s -X POST --data "hashable=$try" http://almamater.xkcd.com/?edu=usu.edu | grep -o 'It is off by .* bits.')
     echo "$try $output"
 done | tee -a tries.txt
